f42ccab35c73d455e93d0c86c3e7642f314804ae
zhanglongde <1039326495@qq.com>
2017-12-17 00:10:14 +0800

    js for 1.data structure 2.leetcode

--- JavaScript-Utilities/heap-size.js
+++
@@ -1,8 +0,0 @@
-function checkHeapSize (lastUsed = 0) {
-  if (window.performance.memory.usedJSHeapSize < lastUsed) {
-    console.log('Garbage collected!');
-  }
-  setTimeout(checkHeapSize, 100, window.performance.memory.usedJSHeapSize)
-}
-
-checkHeapSize()
